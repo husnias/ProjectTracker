ed801f372bef02fe7e33b8bcab8407d9eb024ac6
Husnia Sidiqi <hkpayenda@gmail.com>
2023-05-17 00:13:25 -0400

    Add a new Class

---
+++ src/gitJavaclass/Authorization.java
@@ -0,0 +1,9 @@
+package gitJavaclass;
+
+public class Authorization {
+
+	public static void main(String[] args) {
+		System.out.println("Yayee i did it !!!");
+		
+		}
+	}
---
+++ src/gitJavaclass/anotherClass.java
@@ -0,0 +1,8 @@
+package gitJavaclass;
+
+public class anotherClass {
+	public static void main(String[] args) {
+		
+	}
+
+}
